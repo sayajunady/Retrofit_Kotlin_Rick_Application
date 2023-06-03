a6d3662a11e29ce24353fdc91fef97a36eb22279
KOPI KODE <junadhi@sar.ac.id>
2023-06-04 05:33:10 +0700

    Initial commit

---
+++ app/src/main/java/com/kopikode/rickapplication/DetailRickActivity.kt
@@ -0,0 +1,11 @@
+package com.kopikode.rickapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailRickActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_rick)
+    }
+}
